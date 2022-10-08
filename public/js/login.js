25686ccef1ebb688d5515705f5fab37da59db7ac
Brian McKenna <brianmckennaman@gmail.com>
2022-10-08 15:26:42 -0400

    added code to models, public,utils  and controller folders

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#login-email').value.trim();
+    const password = document.querySelector('#login-password').value.trim();
+
+    if(email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json'},
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Login failed! Please try again.')
+        }
+    }
+};
+
+document
+    .querySelector('.login-form')
+    .assEventListener('submit',
+    loginFormHandler);
